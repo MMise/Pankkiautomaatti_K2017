c710d0646e07cc1ef1b88ff8022a0515ff3e4635
Mikko Jokinen <mikko.jokinen96@gmail.com>
2017-04-12 09:43:53 +0300

    Create pinkoodi.h

---
+++ Tietokanta/pinkoodi.h
@@ -0,0 +1,38 @@
+#ifndef PINKOODI_H
+#define PINKOODI_H
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QString>
+
+class Pinkoodi
+{
+
+public:
+    void rajapintafunktioPinkoodi();
+    static bool createConnection()
+    {
+        QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+        db.setHostName("mysli.oamk.fi");
+        db.setDatabaseName("opisk_t6jomi00");
+        db.setUserName("t6jomi00");
+        db.setPassword("okEyaA8PA6HrAsg6");
+        if (!db.open()) {
+            qDebug() << "Unable to establish a database connection";
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+private:
+    int tarkastaPin();
+    bool pinOk;
+    QString annettupin;
+    QString salasana;
+    QString kortti;
+};
+
+#endif // PINKOODI_H
