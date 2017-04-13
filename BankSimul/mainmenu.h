bf3f923cc5d370952eac3945fcfbe002f3c68579
komuvill <V-V.Komulainen@hotmail.com>
2017-04-13 09:05:39 +0300

    Update mainmenu.h

--- BankSimul/mainmenu.h
+++ BankSimul/mainmenu.h
@@ -2,6 +2,7 @@
 #define MAINMENU_H
 
 #include <QDialog>
+#include <QDebug>
 #include "rahannosto.h"
 #include "saldontarkastus.h"
 #include "tilitapahtumat.h"
@@ -17,16 +18,22 @@ class mainMenu : public QDialog
 public:
     explicit mainMenu(QWidget *parent = 0);
     ~mainMenu();
+    void setData(const QString &kortinNumero);
 public slots:
     void naytaNosto();
     void naytaSaldo();
     void naytaTapahtumat();
+
+private slots:
+
+    void on_pushButtonUlos_clicked();
 
 private:
     Ui::mainMenu *ui;
     rahannosto *nostovalikko;
     saldontarkastus *saldovalikko;
     tilitapahtumat *tapahtumavalikko;
+    QString korttiID;
 };
 
 #endif // MAINMENU_H
