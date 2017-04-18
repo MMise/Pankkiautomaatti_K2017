fd77eb3445974204d2d3c83a218b45d8c481d7a3
Mikko Jokinen <mikko.jokinen96@gmail.com>
2017-04-18 13:34:57 +0300

    Update tietokanta.cpp

--- Tietokanta/tietokanta.cpp
+++ Tietokanta/tietokanta.cpp
@@ -5,8 +5,8 @@ bool Tietokanta::rajapintafunktioTietokanta()
     db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("mysli.oamk.fi");
     db.setDatabaseName("opisk_t6jomi00");
-    db.setUserName("t6jomi00");
-    db.setPassword("okEyaA8PA6HrAsg6");
+    db.setUserName("salaisuus");
+    db.setPassword("salaisuus");
     if (!db.open()) {
         qDebug() << "Unable to establish a database connection";
         return false;
